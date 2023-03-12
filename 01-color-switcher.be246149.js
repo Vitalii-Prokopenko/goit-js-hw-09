4c70478d6ba5963e1364921f6745dc5c62d3e713
Vitalii-Prokopenko <115025319+Vitalii-Prokopenko@users.noreply.github.com>
2023-03-12 18:26:15 +0000

    Deploying to gh-pages from @ Vitalii-Prokopenko/goit-js-hw-09@f3397221cb5f97678e1a4e3309ee2f8ee82eeb7a 🚀

---
+++ 01-color-switcher.be246149.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let e=null,o=!1;t.startBtn.addEventListener("click",(()=>{o||(e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),o=!0)})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.be246149.js.map
