f3397221cb5f97678e1a4e3309ee2f8ee82eeb7a
Vitalii-Prokopenko <doc8259@gmail.com>
2023-03-12 20:25:47 +0200

    01 color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let intervalChangeColor = null;
+let isColorSwitcherActive = false;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const handleStartClick = () => {
+  if (isColorSwitcherActive) {
+    return;
+  }
+
+  intervalChangeColor = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  isColorSwitcherActive = true;
+};
+
+const handleStopClick = () => {
+  clearInterval(intervalChangeColor);
+};
+
+refs.startBtn.addEventListener('click', handleStartClick);
+refs.stopBtn.addEventListener('click', handleStopClick);
