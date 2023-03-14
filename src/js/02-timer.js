cbd3da7b646d6802e22bab5c4d1b592dc1b8d765
Vitalii-Prokopenko <doc8259@gmail.com>
2023-03-14 22:22:46 +0200

    update 02-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,7 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const refs = {
+  datePicker: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('button[data-start]'),
   daysLeft: document.querySelector('span[data-days]'),
   hoursLeft: document.querySelector('span[data-hours]'),
@@ -21,23 +22,39 @@ const options = {
   },
 };
 
-console.log(refs.timerFields);
-console.log(refs.timerBox.style);
+// Inline styles for date picker
 
-// Inline styles for timer
-// const timerStyles = {
-//   display: 'grid',
-//   gridTemplateColumns: 'repeat(4, 100px)',
-//   gridAutoRows: '40px',
-//   columnGap: '40px',
-// };
+refs.datePicker.style.height = '40px';
+refs.datePicker.style.borderRadius = '10px';
+refs.datePicker.style.borderColor = 'teal';
+refs.datePicker.style.color = 'blue';
+refs.datePicker.style.textAlign = 'center';
 
-// refs.timerBox.style = timerStyles;
+// Inline styles for start button
+
+refs.startBtn.style.border = 'none';
+refs.startBtn.style.borderRadius = '10px';
+refs.startBtn.style.width = '100px';
+refs.startBtn.style.height = '40px';
+refs.startBtn.style.backgroundColor = 'crimson';
+
+refs.startBtn.style.color = 'white';
+
+const handleStartBtnClick = event => {
+  refs.startBtn.style.backgroundColor = 'darkRed';
+};
+
+// Inline styles for timer box
 
 refs.timerBox.style.display = 'flex';
 refs.timerBox.style.justifyContent = 'flex-start';
 refs.timerBox.style.gap = '20px';
 refs.timerBox.style.marginTop = '40px';
+
+// Inline styles for timer fields
+
+const fieldColors = ['#EE0034', '#FFB339', '#25CD44', '#8000FF'];
+let fieldNumber = 0;
 
 refs.timerFields.forEach(timerField => {
   timerField.style.display = 'flex';
@@ -46,4 +63,22 @@ refs.timerFields.forEach(timerField => {
   timerField.firstElementChild.style.fontWeight = '600';
   timerField.firstElementChild.style.fontSize = '50px';
   timerField.lastElementChild.style.fontSize = '30px';
+  timerField.style.color = fieldColors[fieldNumber];
+  fieldNumber += 1;
 });
+
+// flatpickr
+
+const inputDate = flatpickr(refs.datePicker, options);
+const selectedDate = inputDate.selectedDates[0];
+const currentDate = new Date();
+
+if (selectedDate < currentDate) {
+  window.alert('Please choose a date in the future');
+} else {
+  refs.startBtn.addEventListener('click', handleStartBtnClick);
+}
+
+console.log(selectedDate);
+console.log(currentDate);
+console.log(selectedDate < currentDate);
